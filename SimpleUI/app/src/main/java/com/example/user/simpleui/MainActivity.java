3354a97346f487ffbf4af919fd6662681f9a8864
miksim0827 <miksim0827@gmail.com>
2016-08-10 11:19:19 +0800

    add layout update

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -8,7 +8,14 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.SimpleAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,51 +23,82 @@ public class MainActivity extends AppCompatActivity {
     EditText editText;
     RadioGroup radioGroup;
     ListView listView;
+    Spinner spinner;
+
 
     String drink= "Black Tea";
+    List<Order> data = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView)findViewById(R.id.textView);
-        editText = (EditText)findViewById(R.id.editText);
-        radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
-        listView = (ListView)findViewById(R.id.listView);
-
-
+        textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        listView = (ListView) findViewById(R.id.listView);
+        spinner = (Spinner) findViewById(R.id.spinner);
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if(checkedId == R.id.BlackTeaRadioButton)
-                {
+                if (checkedId == R.id.BlackTeaRadioButton) {
                     drink = "Black Tea";
-                }
-                else if(checkedId == R.id.GreenTeaRadioButton)
-                {
+                } else if (checkedId == R.id.GreenTeaRadioButton) {
                     drink = "Green Tea";
                 }
             }
         });
 
-        setupListView();
-
+            setListView();
+            setSpinner();
+    }
         private void setListView()
     {
-        String[] data = new String[]("1","2","3","4","5","6","7","8");
+        //String[] data = new String[]("1","2","3","4","5","6","7","8");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        List<Map<String,String>> mapList = new ArrayList<>();
+
+        for(Order order: data)
+        {
+            Map<String, String> item = new HashMap<>();
+
+            item.put("note",order.note);
+            item.put("storeInfo",order.storelnfo);
+            item.put("drink",order.drink);
+
+            mapList.add(item);
+        }
+
+        String[] from = {"note","storeInfo","drink"};
+        int[] to = {R.id.noteTextViwe,R.id.storeInfoTextViewm,R.id.drinkTextViwe};
+        SimpleAdapter adapter = new SimpleAdapter(this,mapList,R.layout.listview_order_item,from,to);
+
         listView.setAdapter(adapter);
     }
-    }
+        private  void setSpinner()
+        {
+            String[] storelnfo = getResources().getStringArray(R.array.storeInfo);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,storelnfo);
+            spinner.setAdapter(adapter);
+        }
+
         public void click(View view)
         {
             String text = editText.getText().toString();
-            text = text + " Order " + drink;
-            textView.setText(text);
+            String result = text + " Order " + drink;
+            textView.setText(result);
 
             editText.setText("");
+
+            Order order = new Order();
+
+            order.note = text;
+            order.drink = drink;
+            order.storelnfo = (String)spinner.getSelectedItem();
+
+            data.add(order);
+            setListView();
         }
 }
