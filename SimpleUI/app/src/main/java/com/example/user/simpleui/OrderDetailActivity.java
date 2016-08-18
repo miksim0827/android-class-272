887294eb92081e768e807b4f7a1e9e990f466b7a
miksim0827 <miksim0827@gmail.com>
2016-08-18 12:29:17 +0800

    create GeoCodingTask

--- SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
@@ -15,47 +15,24 @@ public class OrderDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
 
-        TextView noteTextView = (TextView)findViewById(R.id.noteTextView);
-        TextView storeInfoTextView = (TextView)findViewById(R.id.storeInfoTextView);
-        TextView drinkOrderResultsTextView = (TextView)findViewById(R.id.drinkOrderResultsTextView);
-        final TextView latlngTextView = (TextView)findViewById(R.id.latLngTextView);
+        TextView noteTextView = (TextView) findViewById(R.id.noteTextView);
+        TextView storeInfoTextView = (TextView) findViewById(R.id.storeInfoTextView);
+        TextView drinkOrderResultsTextView = (TextView) findViewById(R.id.drinkOrderResultsTextView);
+        final TextView latlngTextView = (TextView) findViewById(R.id.latLngTextView);
 
         Intent intent = getIntent();
         Order order = intent.getParcelableExtra("order");
         noteTextView.setText(order.getNote());
         storeInfoTextView.setText(order.getStoreInfo());
         String resultText = "";
-        for (DrinkOrder drinkOrder: order.getDrinkOrderList())
-        {
+        for (DrinkOrder drinkOrder : order.getDrinkOrderList()) {
             String lNumber = String.valueOf(drinkOrder.getlNumber());
             String mNumber = String.valueOf(drinkOrder.getmNumber());
             String drinkName = drinkOrder.getDrink().getName();
-            resultText+=drinkName + "  M:" + mNumber + "  L:" + lNumber + "\n";
+            resultText += drinkName + "  M:" + mNumber + "  L:" + lNumber + "\n";
         }
         drinkOrderResultsTextView.setText(resultText);
 
-        final Handler handler = new Handler(new Handler.Callback() {
-            @Override
-            public boolean handleMessage(Message msg) {
-                latlngTextView.setText("123,456");
-                return false;
-            }
-        });
-
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(1000);
-//                    latlngTextView.setText("123,456");
-                    handler.sendMessage(new Message());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        thread.start();
+        (new GeoCodingTask()).execute("");
     }
-
 }
